a59ac928fd21907269539fd820132597645e2888
nikiv78 <nikolay.note3@gmail.com>
2022-10-20 19:16:51 +0300

    example003

---
+++ example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(1, 11); // 0 1 2 3 4 5 ... 9
+Console.WriteLine(numberA);
+int numberC = new Random().Next(2, 10);
+Console.WriteLine(numberC);
+int result = numberA + numberC;
+Console.WriteLine(result);
+
